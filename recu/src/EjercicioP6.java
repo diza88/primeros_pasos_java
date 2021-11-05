b794b9ae4f4af60bd3b6957f36cf48ce5949002c
diza88 <rihud24@gmail.com>
2021-11-05 14:48:29 -0600

    Cambio en el archivo p6

--- recu/src/EjercicioP6.java
+++ recu/src/EjercicioP6.java
@@ -3,6 +3,18 @@ import java.util.Scanner;
 public class EjercicioP6 {
     public static void main(String[] args) {
         Scanner intro = new Scanner(System.in);
-        
+        double numero = 0;
+
+        System.out.println("Ingrese un numero cualquiera:");
+        numero = intro.nextDouble();
+
+        if (numero == 0) {
+            System.out.println("Su numero es cero:");
+        } else if (numero % 2 == 0) {
+            System.out.println("Su numero es positivo");
+        } else {
+            System.out.println("Su numero es negativo");
+        }
+
     }
 }
